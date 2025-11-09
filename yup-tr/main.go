f92c03b63d77e25211ed14a53e4317fea6484703
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:15 -0800

    tests

--- yup-tr/main.go
+++ yup-tr/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/tr"
 )
 
@@ -71,5 +71,5 @@ func action(c *cli.Context) error {
 
 	// Create and execute the tr command
 	cmd := Tr(params...)
-	return yup.Run(cmd)
+	return gloo.Run(cmd)
 }
